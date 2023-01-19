ded6b7f3b0292d4d6210a51fa7af8f7fcb9146ed
chae minkyoung <chea220215@gmail.com>
2023-01-19 18:31:52 +0900

    9.4-2

--- day06 homework.py
+++ day06 homework.py
@@ -1,10 +1,24 @@
+class Oopsexception(Exception):
+    pass
+
 try:
-    numbers=input(f"숫자 두개를 입력하세요:")
-    number=numbers.split()
-    a=int(number[0])
-    b=int(number[1])
-    print(a+b)
-except IndexError as OopsException:
-    print(f'Caught an oops')
+    raise Oopsexception("Caught an oops")
+except Oopsexception as abc:
+    print(abc)
 
 
+
+# # try:
+# #     numbers=input(f"숫자 두개를 입력하세요:")
+# #     number=numbers.split()
+# #     a=int(number[0])
+# #     b=int(number[1])
+# #     print(a+b)
+# # except IndexError as OopsException:
+# #     print(f'Caught an oops')
+# #
+# #
+# def sud(a):
+#     return a**2
+#
+# print(sud(3))
